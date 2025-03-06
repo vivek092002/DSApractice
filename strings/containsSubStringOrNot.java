5af194a941fe398bbc1be030ca355f5118d1ef4d
kumarvivek <kumar.vivek092002@gmail.com>
2025-03-06 23:42:27 +0530

    files added

---
+++ strings/containsSubStringOrNot.java
@@ -0,0 +1,31 @@
+package com.takeyouforward.strings;
+
+public class containsSubStringOrNot {
+    public static void main(String[] args) throws java.lang.Exception {
+        String str = "takeyouforward";
+        String pattern = "forward";
+
+        System.out.println(foundPatter(str,pattern));
+
+    }
+
+    public static int foundPatter(String str, String pattern){
+        int n = str.length();
+        int m = pattern.length();
+
+        for (int i = 0; i < n; i++) {
+            int temp = i;
+            int j = 0;
+            for (j = 0; j < m; j++) {
+                if (str.charAt(temp) != pattern.charAt(j)){
+                    break;
+                }
+                break;
+            }
+            if (j == m){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
