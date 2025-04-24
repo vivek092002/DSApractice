e0895d17f210b0e4cbd6e8c04b13b8ab9acf6724
kumarvivek <kumar.vivek092002@gmail.com>
2025-04-24 23:29:48 +0530

    files added

---
+++ contest/codechef/largerSmaller.java
@@ -0,0 +1,20 @@
+package com.takeyouforward.contest.codechef;
+
+import java.util.Scanner;
+
+public class largerSmaller {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0){
+            int N = sc.nextInt();
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            
+        }
+        sc.close();
+    }
+}
